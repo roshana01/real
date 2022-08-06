24ad89575565fa672480e2aacbbf8e1f62e498d8
roshanak roshani <roshanaroshani99@gmail.com>
2022-08-06 12:26:56 +0430

    add slider

--- assets/script/slider.js
+++ assets/script/slider.js
@@ -1,18 +1,17 @@
-$(".owl-carousel").owlCarousel({
-    loop: false,
-    rewind: true,
-    margin: 5,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    responsive: {
-        0: {
-            items: 1,
-        },
-        600: {
-            items: 2,
-        },
-        1000: {
-            items: 5,
-        },
-    },
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
 });
