bc91f6f43e673172ecd69fb7ae99a71f276266fd
roshanak roshani <roshanaroshani99@gmail.com>
2022-08-17 20:05:18 +0430

    change side-menu script

--- assets/script/app.js
+++ assets/script/app.js
@@ -3,14 +3,13 @@ const menuBtn = document.querySelector(".menu-btn");
 const sideMenu = document.querySelector(".side-menu");
 const sideMenuOverlay = document.querySelector(".side-menu-overlay");
 const closeBtn = document.querySelector(".close-button");
-
+const navlink = document.querySelectorAll(".nav-link");
 window.addEventListener("DOMContentLoaded", () => {
     loader.classList.add("disable");
     setTimeout(() => {
         loader.style.display = "none";
     }, 1000);
 });
-
 
 menuBtn.addEventListener("click", () => {
     sideMenu.classList.add("active");
@@ -20,4 +19,15 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     sideMenu.classList.remove("active");
     sideMenuOverlay.classList.remove("active");
+});
+
+navlink.forEach((items) => {
+    // console.log(items);
+
+    items.addEventListener("click", () => {
+        setTimeout(() => {
+            sideMenu.classList.remove("active");
+            sideMenuOverlay.classList.remove("active");
+        }, 1000);
+    });
 });
